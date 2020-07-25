5d9f5df0d7893ce2c4e1e14d133d0d97b3e806b0
alex2623 <alex2623@naver.com>
2020-07-25 16:28:56 +0900

    fifo code V1.1

--- sequencer_RTIO/fifo_dualclk.sv
+++ sequencer_RTIO/fifo_dualclk.sv
@@ -2,34 +2,33 @@ module fifo_dualclk
 #(
 	parameter
 		DATA_WIDTH = 68,
-		LOGDEPTH = 7
+		LOGDEPTH = 7,
+		DEPTH = 2 ** LOGDEPTH
 )
 (
-	input logic clk, wrst, rrst, clkx8			// clk : main processor clock (100 MHz) , wrst : synchronous reset signal in wirte clock, rrst : synchronous reset signal in read clock, clkx8 : clock which is 8times faster clk ( 800MHz )
+	input logic clk, wrst, rrst, clkx8,		// clk : main processor clock (100 MHz) , wrst : synchronous reset signal in wirte clock, rrst : synchronous reset signal in read clock, clkx8 : clock which is 8times faster clk ( 800MHz )
 	input logic wr_en, rd_en,				// wr_en : write enable, rd_en : read enable
-	input logic [DATA_WIDTH] din,			// data input
-	output logic empty, full				// empty : indicate fifo cannot discharge, full : indicate fifo is full
-	output logic [DATA_WIDTH] dout			// data output
+	input logic [DATA_WIDTH:0] din,			// data input
+	output logic empty, full,				// empty : indicate fifo cannot discharge, full : indicate fifo is full
+	output logic [DATA_WIDTH:0] dout		// data output
 );
-
-integer DEPTH = 2 ** LOGDEPTH;				// depth of FIFO
 
 logic writable;
 logic readable;
 logic array_reg_we;
-logic [DEPTH : 0] read_gray;
-logic [DEPTH : 0] write_gray;
-logic [DEPTH : 0] read_gray_synced;
-logic [DEPTH : 0] write_gray_synced;
-logic [DEPTH - 1:0] read_address;
-logic [DEPTH - 1:0] read_address_next;
-logic [DEPTH - 1:0] write_address;
-logic [DEPTH - 1:0] write_address_next;
+logic [LOGDEPTH : 0] read_gray;
+logic [LOGDEPTH : 0] write_gray;
+logic [LOGDEPTH : 0] read_gray_synced;
+logic [LOGDEPTH : 0] write_gray_synced;
+logic [LOGDEPTH - 1 : 0] read_address;
+logic [LOGDEPTH - 1:0] read_address_next;
+logic [LOGDEPTH - 1:0] write_address;
+logic [LOGDEPTH - 1:0] write_address_next;
 logic [DATA_WIDTH - 1:0] dout_buffer;
 logic [DATA_WIDTH - 1:0] din_buffer;
 logic [DATA_WIDTH - 1:0] array_reg[0:DEPTH-1];
 
-gray_counter #( LOGDEPTH + 1 ) rd_counter(
+gray_counter #( .LENGTH( LOGDEPTH + 1 ) ) rd_counter(
 	.clk_counter(clk),
 	.clk_diff(clkx8),
 	.reset_counter(rrst),
@@ -40,7 +39,7 @@ gray_counter #( LOGDEPTH + 1 ) rd_counter(
 	.gray_out_synced(read_gray_synced)
 );
 
-gray_counter #( LOGDEPTH + 1 ) wr_counter(
+gray_counter #( .LENGTH( LOGDEPTH + 1 ) ) wr_counter(
 	.clk_counter(clkx8),
 	.clk_diff(clk),
 	.reset_counter(wrst),
